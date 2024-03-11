4a64363417c0630fc6657ea6820d0d9214098acd
Rajesh Shenoy <rshenoy@bp-3.com>
2024-03-11 23:36:38 +0000

    added camunda 8 quiz

--- src/main/resources/externalScript2.groovy
+++ src/main/resources/externalScript2.groovy
@@ -4,4 +4,4 @@ def currentTime = new Date()
 def dateFormat = new SimpleDateFormat("HH:mm:ss")
 def formattedTime = dateFormat.format(currentTime)
 
-println "Calling Camunda 7 Quiz .....Current Time: ${formattedTime}"
+println "Calling Camunda 8 Quiz app .........Current Time: ${formattedTime}"
